1dc66f3182e41d25a1ea77ca55c14db9fc19fd40
DevNemoo <allancoutarel@orange.fr>
2022-04-02 14:32:39 +0200

    affichage grille 32x16

--- js/application/game.js
+++ js/application/game.js
@@ -13,11 +13,24 @@ function retry(){
     if (r == true) {
         location.reload();
     }
-  }
+}
 
-  function return_menu(){
+function return_menu(){
     var r =confirm("Voulez-vous retourner à l'accueil ?");
     if (r == true) {
         window.location.href='../index.html';
     }
-  }
+}
+
+function printMap() {
+    const map = document.querySelector("boulderdash");
+    map.innerHTML = "";
+    map.style.setProperty('--grid-rows', 16);
+    map.style.setProperty('--grid-cols', 32);
+    for (let c = 0; c < (32 * 16); c++) {
+        let cell = document.createElement("div");
+        cell.style.backgroundImage = "url('../img/textures/diamond.png')";
+        cell.style.backgroundSize = "cover";
+        map.appendChild(cell).className = "grid-item";
+    }
+}
