9bc7c55fbd424e21fd7cbed3850f98a16e71a1c2
DevNemoo <allancoutarel@orange.fr>
2022-04-02 15:29:59 +0200

    maj des textures

--- js/application/game.js
+++ js/application/game.js
@@ -23,14 +23,46 @@ function return_menu(){
 }
 
 function printMap() {
+    let T = 'T';
+    let V = 'V';
+    let R = 'R';
+    let M = 'M';
+    let P = 'P';
+    let D = 'D';
+    let tab = [
+        [T,T,T,T,R,T,T,T,R,T,T,T,T,V,T,T,T,T,T,T,R,T,T,T,T,V,T,T,T,T,T,T],
+        [T,T,P,T,T,T,T,R,D,T,T,M,M,M,M,M,M,M,M,T,T,T,T,D,T,T,T,T,R,T,T,T],
+        [T,T,T,R,T,T,T,R,V,T,T,T,T,V,T,T,T,T,R,T,R,T,T,T,T,T,T,T,T,T,T,T],
+        [T,T,R,T,T,T,T,R,T,T,T,T,T,T,T,T,R,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T],
+        [T,T,T,T,R,T,T,T,R,T,T,T,T,V,T,T,T,T,T,T,R,T,T,T,T,V,T,T,T,T,T,T],
+        [T,T,R,T,T,T,T,R,D,T,T,M,M,M,M,M,M,M,M,T,T,T,T,D,T,T,T,T,R,T,T,T],
+        [T,T,T,R,T,T,T,R,V,T,T,T,T,V,T,T,T,T,R,T,R,T,T,T,T,T,T,T,T,T,T,T],
+        [T,T,R,T,T,T,T,R,T,T,T,T,T,T,T,T,R,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T],
+        [T,T,T,T,R,T,T,T,R,T,T,T,T,V,T,T,T,T,T,T,R,T,T,T,T,V,T,T,T,T,T,T],
+        [T,T,R,T,T,T,T,R,D,T,T,M,M,M,M,M,M,M,M,T,T,T,T,D,T,T,T,T,R,T,T,T],
+        [T,T,T,R,T,T,T,R,V,T,T,T,T,V,T,T,T,T,R,T,R,T,T,T,T,T,T,T,T,T,T,T],
+        [T,T,R,T,T,T,T,R,T,T,T,T,T,T,T,T,R,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T],
+        [T,T,T,T,R,T,T,T,R,T,T,T,T,V,T,T,T,T,T,T,R,T,T,T,T,V,T,T,T,T,T,T],
+        [T,T,R,T,T,T,T,R,D,T,T,M,M,M,M,M,M,M,M,T,T,T,T,D,T,T,T,T,R,T,T,T],
+        [T,T,T,R,T,T,T,R,V,T,T,T,T,V,T,T,T,T,R,T,R,T,T,T,T,T,T,T,T,T,T,T],
+        [T,T,R,T,T,T,T,R,T,T,T,T,T,T,T,T,R,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T],
+    ];
     const map = document.querySelector("boulderdash");
     map.innerHTML = "";
     map.style.setProperty('--grid-rows', 16);
     map.style.setProperty('--grid-cols', 32);
-    for (let c = 0; c < (32 * 16); c++) {
-        let cell = document.createElement("div");
-        cell.style.backgroundImage = "url('../img/textures/diamond.png')";
-        cell.style.backgroundSize = "cover";
-        map.appendChild(cell).className = "grid-item";
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 32; j++) {
+            let cell = document.createElement("div");  
+            switch (tab[i][j]) {
+                case T: cell.style.backgroundImage = "url('../img/textures/dirt.png')"; break;
+                case V: cell.innerHTML = ""; break;
+                case R: cell.style.backgroundImage = "url('../img/textures/stone.png')"; break;
+                case M: cell.style.backgroundImage = "url('../img/textures/wall.png')"; break;
+                case P: cell.style.backgroundImage = "url('../img/textures/rockford.png')"; break;
+                case D: cell.style.backgroundImage = "url('../img/textures/diamond.png')"; break;
+            }
+            map.appendChild(cell).className = "grid-item";
+        }
     }
 }
